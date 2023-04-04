9c003b08362318368f1a48f9a47eac9a44e52423
1Hbi <hsoon13245@gmail.com>
2023-04-04 10:04:54 +0300

    initial init

---
+++ src/Main.java
@@ -0,0 +1,42 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		
+		//walk(8);	
+		//System.out.println(factoryal(4));
+		System.out.println(power(2, 3));
+	}
+
+	private static int power(int base, int exponent) {
+		 
+		if(exponent < 1)return 1;
+		return base * power(base, exponent - 1);
+		
+		
+		
+		
+	}
+
+	private static int factoryal(int i) {
+	  
+		if(i<1)return 1;
+		return i * factoryal(i-1);
+		
+		
+	}
+
+	private static void walk(int steps) {
+		
+		if(steps<1)return;
+		System.out.println(" step ");
+		steps--;
+		walk(steps);
+		
+		
+		
+		
+	}
+
+}
